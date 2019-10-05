85e15f57a23c751933085d98824086cc6d75b15a
h3li05369 <akashsm@ce.iitr.ac.in>
2019-10-05 14:54:19 +0530

    conflict resolved

--- tfp/config/config.py
+++ tfp/config/config.py
@@ -1,3 +1,24 @@
 import os
 
-SPLIT_JSON_LOC = os.path.join(os.getcwd(),"tfp" ,"config", "split.json")
+SPLIT_JSON_LOC = os.path.join(os.getcwd(),"tfp", "config", "split.json")
+PARENT_LIMBS = {
+        21 : [16, 15, 1, 2, 3, 1, 5, 6, 14, 8, 9, 14, 11, 12, 14, 14, 1, 4, 7, 10, 13],
+        15 : [0, 0, 1, 2, 3, 1, 5, 6, 1, 0, 9, 10, 0, 12,13]
+        }
+
+LIMB_RATIOS = {
+                21 : [2.0, 2.5,
+                       1.37, 2.8, 2.4,
+                       1.37, 2.8, 2.4,
+                       1.05, 4.2, 3.6,
+                       1.05, 4.2, 3.6,
+                       0, 2.25, 0.8,
+                       1.2, 1.2,
+                       2.0, 2.0]
+            }
+ROOT_LIMB = {
+            21: 14
+        }
+HEAD_LIMB = {
+            21:0
+        }
--- tfp/utils/skeletal_utils.py
+++ tfp/utils/skeletal_utils.py
@@ -1,41 +1,43 @@
 from scipy.signal import savgol_filter
 import numpy as np
 
-import skeleton_data as sk_data
-import prior_sk_data as psk_data
 import tfp.config.config as config
 
 
-def smoothen(preds, window=11, order=3, axis=0):
-    return savgol_filter(preds, window, order, axis=axis)
+def smoothen(joints, window=11, order=3, axis=0):
+    return savgol_filter(joints, window, order, axis=axis)
 
 class Transformation:
 
     def __init__(self,number_joints):
         """
         """
-        parent_limbs = 'PARENT_LIMBS_'+str(number_joints)
-        self.parent_limbs = config.parent_limbs
-
+        self.parent_limbs = config.PARENT_LIMBS[number_joints]
+        self.root = config.ROOT_LIMB[number_joints]
+        self.head = config.HEAD_LIMB[number_joints]
+        self.limb_ratios = config.LIMB_RATIOS[number_joints]
 
 
     def _gen_limb_graph(self):
         """
-        Params:
-        li
+        returns a list whose each index i stores its corresponding children
         """
-        n = len(limb_parents)
+        n = len(parent_limbs)
         G = [[] for i in range(n)]
         for i in range(n):
-            j = limb_parents[i]
+            j = parent_limbs[i]
             if i != j:
                 G[j].append(i)
         return G
 
 
-    def _bfs_order(G, root):
+    def _bfs_order(self):
+
         from collections import deque
-        q = deque([root])
+        # Generate the graph
+        G = _gen_limb_graph()
+        # Store the bfs order of the joints
+        q = deque([self.root])
         order = []
         while q:
             u = q.popleft()
@@ -44,87 +46,99 @@ class Transformation:
                 q.append(v)
         return order
 
-    def _get_parent_relative_joint_locations(joints_xyz, sk_data=psk_data):
+    def _get_parent_relative_joint_locations(joints_xyz):
         """
-        Assumed shape: [#limbs, X, ...]
-        """
-        limb_parents = sk_data.limb_parents
-        joints_xyz = joints_xyz.swapaxes(0, axis)
+        Params:
+        joints_xyz: joint locations in different frames in Cartesian coordinates
+        shape : [number_frames,number_joints,3]
 
-        limb_rel_preds = joints_xyz - joints_xyz[limb_parents]
-        return limb_rel_preds
-
-    def _get_abs_joint_locations(rel_joints_xyz, axis=0, sk_data=psk_data):
+        Returns:
+        rel_joints_xyz: location of every joint relative to their respective parents
+        shape : [number_frames,number_joints,3]
         """
-        Assumed shape: [#limbs, X, ...]
-        """
-        limb_parents, limb_order = sk_data.limb_parents, sk_data.limb_order
 
-        abs_preds = np.zeros_like(rel_joints_xyz, dtype=np.float64)
-        abs_preds[limb_order[0]] = rel_joints_xyz[limb_order[0]]
+        rel_joints_xyz = joints_xyz - joints_xyz[:,self.parent_limbs]
+
+        return rel_joints_xyz
+
+    def _get_abs_joint_locations(rel_joints_xyz):
+        """
+        Params:
+        rel_joints_xyz: location of every joint relative to their respective parents
+        shape : [number_frames,number_joints,3]
+
+        Returns:
+        abs_joints_xyz: location of every joint relative to their respective parents
+        shape : [number_frames,number_joints,3]
+        """
+
+
+        abs_joints_xyz = np.zeros_like(rel_joints_xyz, dtype=np.float64)
+        limb_order = _bfs_order()
+        # Restore the initial coordinates of the root joint
+        abs_joints_xyz[:,limb_order[0]] = self.abs_root_xyz
+        # Traverse the bfs tree to calculate the joint coordinates
         for l in limb_order[1:]:
-            p = limb_parents[l]
-            abs_preds[l] = abs_preds[p] + rel_joints_xyz[l]
-        return abs_preds
+            p = parent_limbs[l]
+            abs_joints_xyz[:,l] = abs_joints_xyz[:,p] + rel_joints_xyz[:,l]
+        return abs_joints_xyz
 
 
     def _cart2sph(xyz):
         """
-        Assumed shape: [#limbs,3]
+        Assumed shape: [number_frames,number_joints,3]
         """
         # Hypotenuse of the triangle formed in xy plane.It is required
         # to calculate the azimuth angle(φ)
-        hxy = np.hypot(xyz[:,0],xyz[:,1])
+        hxy = np.hypot(xyz[:,:,0],xyz[:,:,1])
         # Spherical coordinates matrix
         rtp = np.zeros_like(xyz, dtype=np.float64)
         # Radial distance(r)
-        rtp[:,0] = np.linalg.norm(xyz, axis=1)
+        rtp[:,:,0] = np.linalg.norm(xyz, axis=2)
         # Polar angle(θ)
-        rtp[:,1] = np.arctan2(hxy,xyz[:,2])
+        rtp[:,:,1] = np.arctan2(hxy,xyz[:,:,2])
         # Azimuth angle(φ)
-        rtp[:,2] = np.arctan2(xyz[:,1],xyz[:,0])
+        rtp[:,:,2] = np.arctan2(xyz[:,:,1],xyz[:,:,0])
         return rtp
 
 
     def _sph2cart(rtp):
         """
-        Assumed shape: [#limbs,3]
+        Assumed shape: [number_frames,number_joints,3]
         """
         # Cartesian coordinates matrix
         xyz = np.zeros_like(rtp, dtype=np.float64)
         # x coordinates: r * sin(θ) * cos(φ)
-        xyz[:,0] = rtp[:,0] * np.sin(rtp[:,1]) * np.cos(rtp[:,2])
+        xyz[:,:,0] = rtp[:,:,0] * np.sin(rtp[:,:,1]) * np.cos(rtp[:,:,2])
         # y coordinates: r * sin(θ) * sin(φ)
-        xyz[1] = rtp[0] * np.sin(rtp[1]) * np.sin(rtp[2])
+        xyz[:,:1] = rtp[:,:,0] * np.sin(rtp[:,:,1]) * np.sin(rtp[:,:,2])
         # z coordinates: r * cos(θ)
-        xyz[2] = rtp[0] * np.sin(rtp[1])
+        xyz[:,:,2] = rtp[:,:,0] * np.sin(rtp[:,:,1])
         return xyz
 
 
-    def transform(preds, head_length=2.0, sk_data=psk_data):
+    def transform(joints, head_length=2.0):
         """
-        :param preds: joint values of a skeleton or skeleton vector
-        :shape: [#limbs, 3, X, X, ...]
+        Params:
+        joints : the absolute joint coordinates of every frame before normalization
+        shape : [number_frames,number_joints,3]
+
+        Returns:
+        cart_abs_joints : the absolute joint coordinates of every frame after normalization
         """
-        rel_preds = get_parent_relative_joint_locations(preds, sk_data=sk_data)
+        # The absolute Cartesian coordinates of root joints of every frame
+        self.abs_root_xyz = joints[:,self.root]
+        # The Cartesian coordinates of all the joints relative to their respective parents
+        rel_joints_xyz = get_parent_relative_joint_locations(joints)
+        # The unnormalized joint coordinates in Spherical coordinate system
+        sph_rel_joints = cart2sph(rel_joints_xyz)
+        # normalization of limb lengths
+        fixed_limb_lengths = head_length * self.limb_ratios
+        # Replace the limb lengths with unnormalized limb lengths
+        sph_rel_joints[:, 0] = fixed_limb_lengths
+        # Convert Spherical coordinates back to Cartesian coordinates
+        cart_rel_joints = sph2cart(sph_rel_joints)
+        # Recover the absolute coordinates
+        cart_abs_joints = get_abs_joint_locations(cart_rel_joints, sk_data=sk_data)
 
-        sph_rel_preds = cart2sph(rel_preds)
-
-        fixed_limb_lengths = head_length * sk_data.limb_ratios
-
-        sph_rel_preds[:, 0] = fixed_limb_lengths
-
-        cart_rel_preds = sph2cart(sph_rel_preds)
-
-        cart_abs_preds = get_abs_joint_locations(cart_rel_preds, sk_data=sk_data)
-
-        return cart_abs_preds
-
-
-    def fit_skeleton_frames(preds, head_length=2.0, sk_data=psk_data):
-        return fit_skeleton_frame(preds.transpose([1, 2, 0]) , head_length, sk_data).transpose([2, 0, 1])
-
-
-    def scale_local_skeleton(preds, head_length=2.0):
-        fixed_limb_lengths = np.expand_dims(head_length * psk_data.limb_ratios, axis=1)
-        return preds * fixed_limb_lengths
+        return cart_abs_joints
